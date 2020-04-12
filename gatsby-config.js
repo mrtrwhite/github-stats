55c28ea348eb580d379e26ffa4583f1caa77ffc0
Tim White <tim@theaccountancycloud.com>
2020-04-12 15:12:12 +0100

    Update config for gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Github Stats`,
+    description: ``,
+    author: `@mrtrwhite`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -30,4 +30,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
+  pathPrefix: '/github-stats'
 }
