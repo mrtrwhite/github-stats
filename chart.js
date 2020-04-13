d899c21c77aac7baac55912238fd1d37355d569a
Tim White <tim@theaccountancycloud.com>
2020-04-13 11:19:44 +0100

    Updates

--- chart.js
+++ chart.js
@@ -1,69 +1,77 @@
-var commits = window.apiData.map((item) => {
-    return {
-        label: item.name,
-        x: item.lastCommitDate,
-        y: item.stars
-    }
-});
-
-var releases = window.apiData.filter((item) => {
-        return item.lastRelease;
-    })
-    .map((item) => {
-        return {
-            label: item.name,
-            x: item.lastCommitDate,
-            y: item.stars
+axios.get('/page-data/index/page-data.json')
+    .then((response) => {
+        if(typeof response.data.result.pageContext.data === undefined) {
+            return;
         }
-    });
 
-var chart = new Chart(document.getElementById('chart'), {
-    type: 'scatter',
-    data: {
-        datasets: [
-            {
-                label: 'Latest Commits',
-                data: commits
-            },
-            {
-                label: 'Latest Releases',
-                data: releases,
-                hidden: true
-            }
-        ]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: true,
-        scales: {
-            yAxes: [
-                {
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Stars'
-                    }
-                }
-            ],
-            xAxes: [
-                {
-                    type: 'time',
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Time'
+        var apiData = response.data.result.pageContext.data;
+
+        var chart = new Chart(document.getElementById('chart'), {
+            type: 'scatter',
+            data: {
+                datasets: [
+                    {
+                        label: 'Latest Commits',
+                        data: apiData.map((item) => {
+                            return {
+                                label: item.name,
+                                x: item.lastCommitDate,
+                                y: item.stars
+                            }
+                        })
                     },
-                    time: {
-                        tooltipFormat: 'Do MMMM YYYY',
-                        unit: 'month'
+                    {
+                        label: 'Latest Releases',
+                        data: apiData.filter((item) => {
+                            return item.lastRelease;
+                        })
+                        .map((item) => {
+                            return {
+                                label: item.name,
+                                x: item.lastCommitDate,
+                                y: item.stars
+                            }
+                        }),
+                        hidden: true
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                scales: {
+                    yAxes: [
+                        {
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Stars'
+                            }
+                        }
+                    ],
+                    xAxes: [
+                        {
+                            type: 'time',
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Time'
+                            },
+                            time: {
+                                tooltipFormat: 'Do MMMM YYYY',
+                                unit: 'month'
+                            }
+                        }
+                    ]
+                },
+                tooltips: {
+                    callbacks: {
+                        label: (tooltipItem, data) => {
+                            return data.datasets[tooltipItem.datasetIndex]['data'][tooltipItem.index].label;
+                        }
                     }
                 }
-            ]
-        },
-        tooltips: {
-            callbacks: {
-                label: (tooltipItem, data) => {
-                    return data.datasets[tooltipItem.datasetIndex]['data'][tooltipItem.index].label;
-                }
             }
-        }
-    }
-})
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
